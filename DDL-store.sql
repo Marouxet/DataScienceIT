c3a1560b44de96e8548f4569953955afc62afcd5
marouxet <marianoarouxet@gmail.com>
2021-02-10 16:19:38 -0300

    clase 03 actualizada

--- DDL-store.sql
+++ DDL-store.sql
@@ -1,112 +1,92 @@
-/*  
-Comentario 
-Multilínea
-*/
+/* creacion base de datos */
+	DROP DATABASE IF EXISTS store; -- eliminar
+	CREATE DATABASE store; -- crear
+	USE store; -- utilizar
 
-## Comentario mysql: solo funciona en mysql
--- Comentario SQL: funciona en cualquier motor SQL
-
-/* creación de base de datos
-drop database if exist [nombre]; -- eliminar
-create database [nombre]; --  crear
-use [nombre]; -- usar
-
-/*
-
-/* creacion de tabla */
-
-create database store_one; --  crear
-
-use store_one;
-## TABLA CLIENTES
-create table clientes(
-	id_cliente int auto_increment,
-	apellido varchar(50), 
-	nombre varchar(50),
-    tipo_doc enum('dni','dni ext.','ci','lc','le','pass') not null,
-    num_doc int not null,
-    fecha_nac date,
-    tel varchar(20),
-    email varchar(200),
-    direccion varchar(200),
-    primary key(id_cliente),
-    unique key(tipo_doc, num_doc)
-);
-
-## TABLA PROVEEDORES
-create table proveedores(
-	id_proveedor int auto_increment,
-    proveedor varchar(100),
-    cuil char(13), -- nn-nnnnnnnn-n
-    contacto varchar(50),
-	tel varchar(20),
-    direccion varchar(100),
-    primary key(id_proveedor),
-    unique key(cuil)
-);
-
-## TABLA PRODUCTOS
-
-create table productos(
-	id_producto int auto_increment,
-    articulo varchar(20) not null,
-    codigo varchar(20) not null, 
-    marca varchar(50),
-    categoria enum('tecno','hogar','higiene','alimento'),
-    stock int not null,
-    precio int not null,
-    valoracion enum('1','2','3','4','5'),
-    detalle varchar(200),
-    id_proveedor int not null,
-    primary key(id_producto),
-    unique key(codigo, id_proveedor),
-    foreign key (id_proveedor) references proveedores(id_proveedor) -- sintaxis :foreign key (var_local) references tabla_foranea(var_foraneo)
-);
-
-/* Modificar una tabla */
-alter table clientes
-	add sexo enum('m','f') after nombre, -- agregar un campo luego de otro campo
-	drop tel, -- borrar un campo
-	modify sexo enum ('m','f','o'), -- modificar un tipo de campo
-	rename column sexo to genero; -- renombrar una columna 
+/*creacion de tablas*/
+	## CLIENTES
+    CREATE TABLE clientes(
+		id_cliente INT AUTO_INCREMENT, -- clave principal autonumerica
+        apellido VARCHAR(50), -- texto variable
+        nombre VARCHAR(50), -- hasta 50 caracteres
+        tipo_doc ENUM('dni','dni ext.','ci','lc','le','pass') NOT NULL,
+        sexo ENUM('m','f'),
+        num_doc INT NOT NULL,
+        fecha_nac DATE,
+        tel VARCHAR(20),
+        email VARCHAR(200),
+        direccion VARCHAR(200),
+        PRIMARY KEY(id_cliente), -- campo relacional (vincula con otras tablas)
+        UNIQUE KEY(tipo_doc, sexo, num_doc) -- campo/s unico/s local/es
+    );
+	-- modificar tablas
+	ALTER TABLE clientes
+		ADD observaciones varchar(200), -- agrega un campo a la tabla
+		DROP observaciones, -- elimina un campo de la tabla
+		MODIFY sexo ENUM("m","f","o"); -- cambia un tipo de campo si es posible
+	
+    ## PROVEEDORES
+	CREATE TABLE proveedores(
+		id_proveedor INT AUTO_INCREMENT,
+        proveedor VARCHAR(100),
+        cuil CHAR(13), -- 99-99999999-9
+        contacto VARCHAR(50),
+        tel VARCHAR(20),
+        direccion VARCHAR(100),
+		PRIMARY KEY(id_proveedor),
+        UNIQUE KEY(cuil)
+    );
+    ALTER TABLE proveedores	
+		ADD email VARCHAR(100) AFTER contacto;
     
- /* Agregamos mail a la tabla proveedores*/
- 
- alter table proveedores
-	add email varchar(100) after tel;
-    
-    
-    
-## Facturación (Tabla para vincular clientes -> compras)
-	create table facturacion (
-		id_factura int auto_increment,
-        id_cliente int,
-        tipo enum ("debe","haber"),
-        tipo_pago enum(
+    ## PRODUCTOS
+    DROP TABLE IF EXISTS productos;
+    CREATE TABLE productos(
+		id_producto INT AUTO_INCREMENT,
+        articulo VARCHAR(50),
+        codigo VARCHAR(20),
+        categoria ENUM('tecnologia', 'hogar', 'higiene', 'alimentos'),
+        stock INT NOT NULL,
+        precio INT NOT NULL,
+        valoracion ENUM("1","2","3","4","5"),
+        detalle VARCHAR(200),
+        id_proveedor INT NOT NULL,
+        PRIMARY KEY(id_producto),
+        UNIQUE KEY(codigo),
+        FOREIGN KEY(id_proveedor) REFERENCES proveedores(id_proveedor) -- relacion externa (campo_local) tabla_ext(campo_ext)
+	);
+    ALTER TABLE productos
+		MODIFY codigo VARCHAR(20) NOT NULL;
+	
+    ## FACTURACION (clientes => Compras)
+	CREATE TABLE facturacion(
+		id_factura INT AUTO_INCREMENT,
+        id_cliente INT,
+        tipo ENUM ("debe", "haber"),
+        tipo_pago ENUM(
 			"efectivo",
-            "debito",
+			"debito",
             "credito",
             "virtual"
-            ),
-		monto int,
-        impuesto decimal(5,4),
-        fecha date default(current_date()),
-        primary key(id_factura),
-        foreign key(id_cliente) references clientes(id_cliente) 
+        ),
+        monto INT,
+        impuesto DECIMAL(5,4), 
+        fecha DATE DEFAULT (CURRENT_DATE()),
+        PRIMARY KEY(id_factura),
+        FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
+    );
+    ## FACTURACION DETALLE (productos => compras)
+	CREATE TABLE facturacion_detalle(
+		id_detalle INT AUTO_INCREMENT,
+        id_factura INT,
+        id_producto INT,
+        cantidad INT,
+		precio INT,
+        PRIMARY KEY(id_detalle),
+        UNIQUE KEY(id_factura, id_producto),
+        FOREIGN KEY(id_factura) REFERENCES facturacion(id_factura),
+        FOREIGN KEY(id_producto) REFERENCES productos(id_producto)
     );
     
-    
-   ## facturacion detalle 
-   create table facturacion_detalle (
-	id_detalle int auto_increment,
-    id_factura int,
-    id_producto int,
-    cantidad int,
-    precio int,
-    primary key (id_detalle),
-    
-   );
-show tables;
-describe clientes;
-select * from clientes;
-drop table productos;
+    SHOW TABLES; 
+    DESCRIBE clientes;
--- DML-store.sql
+++ DML-store.sql
@@ -1,87 +1,102 @@
-/*CARGA DE DATOS CLIENTES*/
-
-/*Carga completa: */
-use store_one;
-insert into clientes values ( -- Requiere todos los parámetros 
-		0, -- id_cliente
-		'Racedo', -- apellido
-		'Abel', -- nombre
-		1, -- tipo_doc (1: dni, 2:dni ext...etc)
-		20048118, -- num_doc
-		'1993-01-05', -- fecha_nacimiento yyyy-mm-dd
-		'11-0303456', -- telefono
-		"abel.racedo365@hotmail.com.ar", -- mail
-		" Avenida siempreviva 55, Springfield" -- Direccion
-); 
-
-/*Carga selectiva*/
-use store_one;
-insert into clientes(apellido, nombre, fecha_nac, tipo_doc, num_doc)
-	values('Arouxet', 'Mariano', '1990/12/01', 1, 35796226);
+USE store;
+/*CARGA DE DATOS - CLIENTES*/
+	## carga completa
+	INSERT INTO clientes
+    values (
+		0, 			-- id_cliente
+        "Racedo",	-- apellido
+        "Abel", 	-- nombre
+        1, 			-- tipo_doc (1: dni, 2: dni ext., 3: ci, 4: lc, 5: le, 6:pass)
+		1, 			-- sexo (1:m, 2:f)
+        38383838, 	-- num_doc
+        "1993-10-12",		-- fecha_nac (yyyy-mm-dd)
+        "(11)-0303-4560",	-- tel (texto)
+        "abel.racedo@gmail.com",	-- email
+        "su casa 93, Temperley"		-- direccion
+        );
+	## carga selectiva
+	INSERT INTO clientes(apellido, nombre, fecha_nac, tipo_doc, num_doc) -- elegimos los campos
+		VALUES('Racedo', 'Cristian', '1991-04-18', 1, 35335335); -- valores
+	SELECT * FROM clientes;
+    ## carga multiple
+	INSERT INTO clientes(apellido, nombre, sexo, tipo_doc, num_doc)
+		VALUES 
+			("Gomez","Gonzalo", 1, 3, 27272727),
+			("Lopez","Lorena", 2, 4, 32323232),
+            ("Perez","Perla", 2, 1, 19191919),
+            ("Diaz","Diana", 1, 2, 38383838),
+            ("Gimenez","Gerardo", 1, 5, 35353535);
+	SELECT * FROM clientes;
+/* CARGA DE DATOS - PROVEEDORES */
+	INSERT INTO proveedores (proveedor, cuil)
+		VALUES
+			("HP","30-54203455-2"),
+			("ASUS","20-07605519-0"),
+			("COMFORT","30-71129323-6"),
+			("SAMSUNG","3068412579-2"),
+			("MOTOROLA","30-71124798-6"),
+			("CANON","30-52030624-9"),
+			("COCA COLA","30-51757696-0"),
+			("CRIOLLITA","30-50927711-3");
+	SELECT * FROM proveedores;
     
-/* Carga  multiple */
-use store_one;
-insert into clientes(apellido, nombre, tipo_doc, num_doc)
-	values
-		("Gomez","Matías",1, 37258215),
-		("Carrillo","Lucas",2 ,11358625 ),
-        ("García","Carlos", 3, 20011758 ),
-        ("Blaufuks","Alejandra", 5, 20011726),
-        ("Gimenez","Susana", 1, 1254852 );
-        
-        
-/*CARGA DE DATOS PROVEEDORES*/
-use store_one;
-insert into proveedores(proveedor,cuil)
-	values
-		("HP","30-71480937-3"),
-        ("ASUS","20-34755132-6"),
-        ("COMFORT","30-59275330-4"),
-        ("MOTOROLA","30-71124798-6"),
-        ("CANNON", "30-61697108-1"),
-        ("COCA COLA","30-51757696-0"),
-        ("SANSUNG","30-68412579-2"),
-        ("MAROLIO","30-71130359-2");
-        
-/*CARGA DE DATOS PROVEEDORES*/
-use store_one;
-insert into productos(articulo,codigo,categoria,valoracion,id_proveedor, stock, precio)
-	values
-    ("aire acondicionado","0552823",2,3,7,500,4500),
-	("gaseosa","5482",4,3,2,10,120),
-    ("impresora","055234",1,5,1,500,4500),
-	("notebook","000078",1,3,1,10,12500),
-    ("colchon","058dda",3,1,5,500,7500),
-	("somiere","9999d",3,2,5,10,4600),
-    ("televisor","144d22",3,4,7,500,16000),
-	("telefono celular","00002",1,5,4,10,24000),
-    ("telefono celular","A558df",1,1,7,500,1400),
-	("notebook","68ds32",1,3,2,10,12500);
-    
-select * from productos;
+/*CARGA DE DATOS - PRODUCTOS*/
+	INSERT INTO productos(articulo, codigo, categoria, valoracion, id_proveedor)
+		VALUES 
+			("aire acond.","0011", 2, 2, 4),
+			("gaseosa", "0012",4, 3, 7),
+			("colchon","0013", 2, 3, 6),
+			("televisor", "0014",1, 4, 4),
+			("impresora","0015", 1, 5, 4),
+			("impresora", "0016",1, 2, 5),
+			("heladera","0017", 2, 3, 2),
+			("microondas", "0018",2, 3, 5);
+	SELECT * FROM productos;
+    TRUNCATE productos;
+/*CARGA DE DATOS - FACTURACION*/
+	INSERT INTO facturacion (id_cliente, tipo, tipo_pago)
+    VALUES 
+		(1,2,3),
+        (4,2,4),
+        (2,2,2),
+        (5,2,1),
+        (6,2,3),
+        (5,2,3);
+/*CARGA DE DATOS - FACTURACION DETALLE*/
+    INSERT INTO facturacion_detalle(id_factura, id_producto, cantidad)
+	VALUES
+		(1, 1, 3), (1, 2, 2), (1, 5, 1), (1, 6, 4),	(1, 10, 2),	-- factura 1
+        (2, 20, 12), (2, 19, 2), (2, 9, 1), 					-- factura 2
+        (3, 8, 2), (3, 6, 2), (3, 3, 1), (3, 2, 1),				-- factura 3
+		(4, 4, 6),												-- factura 4
+		(5, 5, 2), (5, 3, 1); 									-- factura 5
+	SELECT * FROM facturacion_detalle;
+
+/* CONSULTAS DE DATOS */
+	## productos con valoracion 3 ordenados por nombre, solo 5 a partir del producto 5 (6-10)
+	SELECT * FROM productos
+		WHERE valoracion = 3
+        ORDER BY articulo DESC -- orden de la consulta default(ASC)
+        LIMIT 5 -- limite de resultados X para mostrar
+        OFFSET 5; -- a partir de la posicion X en adelante
+
+	## producto con mas de 3 estrellas
+    SELECT id_producto, articulo, codigo, categoria, valoracion -- campos seleccionados
+    FROM productos -- tabla consultada
+    WHERE valoracion > 3 -- condicion
+    ORDER BY valoracion DESC; -- organizacion
+
+	## proveedores que empiecen con la letra C
+	SELECT proveedor, cuil
+    FROM proveedores
+    WHERE proveedor LIKE "C%"; -- "%": cualquier cantidad de caracteres
+
+	## Clientes cuyo apellido termina con Z y cuya segunda letra sea o
+    SELECT apellido, nombre, tipo_doc, num_doc
+    FROM clientes
+    WHERE apellido LIKE "%z" AND nombre LIKE "_o%"; -- "_": un caracter
 
 
 
-/* CONSULTA DE DATOS*/
-	## EJEMPLO: traaer todo con ciertas condiciones
-    
-	select * from productos -- que campos queremos traer y de qué tabla
-		where valoracion > 3 -- condicion
-        order by articulo DESC -- ordenado por stock descendente (el orden por defecto es ascendente)
-        limit 5 -- cantidad de resultados que queremos ver
-        offset 1; -- no mostrar los primeros 5
-        
-	## EJEMPLO: productos con más de  3 estrellas
-    
-	select id_producto, articulo, codigo, categoria, valoracion 
-		from productos
-        where valoracion >= 3
-        order by valoracion desc;
-        
-	## EJEMPLO: proveedores con letra "c"
-    select * from proveedores
-		where  proveedor like "c%"; -- %: es un comodin que indica cualquier cosa 
-        
-	## EJEMPLO: clientes con apellido que terminan con "z" y el nombre tenga como 2da letra la "o"
-    select apellido, nombre, tipo_doc, num_doc from clientes
-		where apellido like "%z" and nombre like "_a%"; -- _: es un comodin que indica un caracter
+
+
